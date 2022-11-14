cbd58b2f6304a5f4158a263644d5c8fc45bf5138
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-11-13 21:07:03 -0300

    Add boilerplate

---
+++ main.js
@@ -0,0 +1,14 @@
+const gameBoard = (function () {
+  // code
+  return {/* public methods */};
+})();
+
+const displayController = (function () {
+  // code
+  return {/* public methods */};
+})();
+
+const Player = function () {
+  //code
+  return {};
+};
