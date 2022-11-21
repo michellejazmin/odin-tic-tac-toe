b0302e1786673bdf9e21e2810505ca7d7161460e
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-11-21 20:39:15 -0300

    Add option to change players' names

--- main.js
+++ main.js
@@ -57,29 +57,29 @@ const gameBoard = (function () {
   }
 
   const checkResult = () => {
-    if (_board[0] === 'X' && _board[1] === 'X' && _board[2] === 'X') return "Player 1 wins the game";
-    else if (_board[0] === 'O' && _board[1] === 'O' && _board[2] === 'O') return "Player 2 wins the game";
+    if (_board[0] === 'X' && _board[1] === 'X' && _board[2] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[0] === 'O' && _board[1] === 'O' && _board[2] === 'O') return `${gameFlow.player2.name} wins the game`;
 
-    else if (_board[3] === 'X' && _board[4] === 'X' && _board[5] === 'X') return "Player 1 wins the game";
-    else if (_board[3] === 'O' && _board[4] === 'O' && _board[5] === 'O') return "Player 2 wins the game";
+    else if (_board[3] === 'X' && _board[4] === 'X' && _board[5] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[3] === 'O' && _board[4] === 'O' && _board[5] === 'O') return `${gameFlow.player2.name} wins the game`;
 
-    else if (_board[6] === 'X' && _board[7] === 'X' && _board[8] === 'X') return "Player 1 wins the game";
-    else if (_board[6] === 'O' && _board[7] === 'O' && _board[8] === 'O') return "Player 2 wins the game";
+    else if (_board[6] === 'X' && _board[7] === 'X' && _board[8] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[6] === 'O' && _board[7] === 'O' && _board[8] === 'O') return `${gameFlow.player2.name} wins the game`;
 
-    else if (_board[0] === 'X' && _board[3] === 'X' && _board[6] === 'X') return "Player 1 wins the game";
-    else if (_board[0] === 'O' && _board[3] === 'O' && _board[6] === 'O') return "Player 2 wins the game";
+    else if (_board[0] === 'X' && _board[3] === 'X' && _board[6] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[0] === 'O' && _board[3] === 'O' && _board[6] === 'O') return `${gameFlow.player2.name} wins the game`;
 
-    else if (_board[1] === 'X' && _board[4] === 'X' && _board[7] === 'X') return "Player 1 wins the game";
-    else if (_board[1] === 'O' && _board[4] === 'O' && _board[7] === 'O') return "Player 2 wins the game";
+    else if (_board[1] === 'X' && _board[4] === 'X' && _board[7] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[1] === 'O' && _board[4] === 'O' && _board[7] === 'O') return `${gameFlow.player2.name} wins the game`;
 
-    else if (_board[2] === 'X' && _board[5] === 'X' && _board[8] === 'X') return "Player 1 wins the game";
-    else if (_board[2] === 'O' && _board[5] === 'O' && _board[8] === 'O') return "Player 2 wins the game";
+    else if (_board[2] === 'X' && _board[5] === 'X' && _board[8] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[2] === 'O' && _board[5] === 'O' && _board[8] === 'O') return `${gameFlow.player2.name} wins the game`;
 
-    else if (_board[0] === 'X' && _board[4] === 'X' && _board[8] === 'X') return "Player 1 wins the game";
-    else if (_board[0] === 'O' && _board[4] === 'O' && _board[8] === 'O') return "Player 2 wins the game";
+    else if (_board[0] === 'X' && _board[4] === 'X' && _board[8] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[0] === 'O' && _board[4] === 'O' && _board[8] === 'O') return `${gameFlow.player2.name} wins the game`;
 
-    else if (_board[2] === 'X' && _board[4] === 'X' && _board[6] === 'X') return "Player 1 wins the game";
-    else if (_board[2] === 'O' && _board[4] === 'O' && _board[6] === 'O') return "Player 2 wins the game";
+    else if (_board[2] === 'X' && _board[4] === 'X' && _board[6] === 'X') return `${gameFlow.player1.name} wins the game`;
+    else if (_board[2] === 'O' && _board[4] === 'O' && _board[6] === 'O') return `${gameFlow.player2.name} wins the game`;
 
     else if (gameFlow.getCounter() > 9) return "It's a tie";
 
@@ -98,18 +98,20 @@ const gameFlow = (function () {
   let _gameIsStarted = false;
   let _gameIsFinished = false;
   let _roundCounter = 0;
-
-  const player1 = Player('Player 1', 'X');
-  const player2 = Player('Player 2', 'O');
   
   const _playerTurnDisplay = document.getElementById('player-turn');
   const _winnerDisplay = document.getElementById('game-result');
+
+  const _player1Input = document.getElementById('player1');
+  const _player2Input = document.getElementById('player2');
+  const player1 = Player('Player 1', 'X');
+  const player2 = Player('Player 2', 'O');
   
   const _showTurn = () => {
     if (_roundCounter % 2 === 0) {
-      _playerTurnDisplay.textContent = "Player 2's turn";
+      _playerTurnDisplay.textContent = `${player2.name}'s turn`;
     } else {
-      _playerTurnDisplay.textContent = "Player 1's turn";
+      _playerTurnDisplay.textContent = `${player1.name}'s turn`;
     }
   };
   
@@ -124,21 +126,32 @@ const gameFlow = (function () {
       _playerTurnDisplay.textContent = "";
     }
   };
+  
+  const newGame = (event) => {
+    event.preventDefault();
+    const formContainer = document.getElementById('form-container');
+    const gameboardContainer = document.getElementById('gameboard-container');
 
-  const newGame = () => {
     if (_gameIsStarted) {
       gameBoard.reset();
     }
+
     _gameIsStarted = true;
     _gameIsFinished = false;
     _roundCounter = 0;
     addToCounter();
+
+    if (_player1Input.value) player1.name = _player1Input.value;
+    if (_player2Input.value) player2.name = _player2Input.value;
+
+    gameboardContainer.classList.remove('hidden');
+    formContainer.classList.add('hidden');
     _winnerDisplay.textContent = "";
-    _playerTurnDisplay.textContent = "Player 1's turn";
+    _playerTurnDisplay.textContent = `${player1.name}'s turn`;
   };
   
   const _startGameButton = document.getElementById('start-game-btn');
-  _startGameButton.addEventListener('click', newGame);
+  _startGameButton.addEventListener('click', newGame, false);
 
   const addToCounter = () => ++_roundCounter;
   const getCounter = () => _roundCounter;
@@ -152,19 +165,13 @@ const gameFlow = (function () {
     getCounter,
     gameIsStarted,
     gameIsFinished,
-    showResult
+    showResult,
+    player1,
+    player2
   };
 })();
 
 function Player(name, piece) {
-  let _isActive = false;
-  const toggleActive = () => isActive = !isActive;
-
-  const playRound = () => {
-    gameFlow.addToCounter();
-
-  };
-
   return {
     name,
     piece
